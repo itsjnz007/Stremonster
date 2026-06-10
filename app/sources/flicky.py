e79b2f31b5eecd08319ce37bd96de746e6d1822f
Jenish <jnz18.mail@gmail.com>
2026-06-10 22:33:52 +0530

    fix

--- app/sources/flicky.py
+++ app/sources/flicky.py
@@ -16,14 +16,14 @@ class FlickyScraper(Scraper):
 
     def get_movie(self, tmdb_id: str) -> Optional[WebResponse]:
         # url = f"{self.base_url}/player/movie/{tmdb_id}"
-        url = f"{self.base_url}/movies4f/movie/{tmdb_id}"
+        url = f"{self.base_url}/movie/{tmdb_id}"
         result = self.get_stream(url)
         if result: result['url'] = Proxy.get_proxy_url(result['url'], origin=self.base_url)
         return result
     
     def get_series(self, tmdb_id: str, season: str, episode: str) -> Optional[WebResponse]:
         # url = f"{self.base_url}/player/tv/{tmdb_id}/{season}/{episode}"
-        url = f"{self.base_url}/movies4f/tv/{tmdb_id}/{season}/{episode}"
+        url = f"{self.base_url}/tv/{tmdb_id}/{season}/{episode}"
         result = self.get_stream(url)
         if result: result['url'] = Proxy.get_proxy_url(result['url'], origin=self.base_url)
         return result
