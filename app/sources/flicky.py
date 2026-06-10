190c3db955e0ecabf2d52b05eed04cea019a8dce
Jenish <jnz18.mail@gmail.com>
2026-06-10 22:26:48 +0530

    updated vidnest

--- app/sources/flicky.py
+++ app/sources/flicky.py
@@ -11,16 +11,19 @@ from typing import Optional
 class FlickyScraper(Scraper):
     def __init__(self):
         super().__init__(headless=True, source="flicky")
-        self.base_url = "https://flickystream.su"
+        # self.base_url = "https://flickystream.su"
+        self.base_url = "https://new.vidnest.fun"
 
     def get_movie(self, tmdb_id: str) -> Optional[WebResponse]:
-        url = f"{self.base_url}/player/movie/{tmdb_id}"
+        # url = f"{self.base_url}/player/movie/{tmdb_id}"
+        url = f"{self.base_url}/movies4f/movie/{tmdb_id}"
         result = self.get_stream(url)
         if result: result['url'] = Proxy.get_proxy_url(result['url'], origin=self.base_url)
         return result
     
     def get_series(self, tmdb_id: str, season: str, episode: str) -> Optional[WebResponse]:
-        url = f"{self.base_url}/player/tv/{tmdb_id}/{season}/{episode}"
+        # url = f"{self.base_url}/player/tv/{tmdb_id}/{season}/{episode}"
+        url = f"{self.base_url}/movies4f/tv/{tmdb_id}/{season}/{episode}"
         result = self.get_stream(url)
         if result: result['url'] = Proxy.get_proxy_url(result['url'], origin=self.base_url)
         return result
