7c26eb8c6f8d12dac07145a36752102fe90312e1
Jenish <jnz18.mail@gmail.com>
2026-06-17 20:35:30 +0530

    fixes

--- app/core/server.py
+++ app/core/server.py
@@ -25,7 +25,7 @@ from threading import Event
 logger = Logger("server")
 app = Flask(__name__)
 
-thread_pool_web = MultiThreading(max_workers=3)
+thread_pool_web = MultiThreading(max_workers=4)
 thread_pool_torrent = MultiThreading(max_workers=3)
 
 tmdb_cache = TmdbCache()
@@ -134,16 +134,16 @@ def get_web_stream(type: str, id: str) -> Response:
         
         # Registries for Scrapers
         movie_scrapers: List[Tuple[Callable[[Event, str], Optional[WebResponse]], str]] = [
-            (lambda event, tmdb_id: flicky_scraper.get_movie(tmdb_id, event), 'flicky'),
             (lambda event, tmdb_id: vidking_scraper.get_movie(tmdb_id, event), 'vidking'),
             (lambda event, tmdb_id: vidsrc_scraper.get_movie(tmdb_id, event), 'vidsrc'),
+            (lambda event, tmdb_id: flicky_scraper.get_movie(tmdb_id, event), 'flicky'),
             (lambda event, tmdb_id: cineby_scraper.get_movie(tmdb_id, event), 'cineby'),
         ]
 
         series_scrapers: List[Tuple[Callable[[Event, str, str, str], Optional[WebResponse]], str]] = [
-            (lambda event, tmdb, s, e: flicky_scraper.get_series(tmdb, s, e, event), 'flicky'),
             (lambda event, tmdb, s, e: vidking_scraper.get_series(tmdb, s, e, event), 'vidking'),
             (lambda event, tmdb, s, e: vidsrc_scraper.get_series(tmdb, s, e, event), 'vidsrc'),
+            (lambda event, tmdb, s, e: flicky_scraper.get_series(tmdb, s, e, event), 'flicky'),
             (lambda event, tmdb, s, e: cineby_scraper.get_series(tmdb, s, e, event), 'cineby'),
         ]
 
