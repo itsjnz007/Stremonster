f87267ceeb134ed40478dbf901e56562ea4e9181
Jenish <jnz18.mail@gmail.com>
2026-06-21 15:19:54 +0530

    fixes

--- app/sources/regional/tamilblasters.py
+++ app/sources/regional/tamilblasters.py
@@ -79,5 +79,5 @@ if __name__ == "__main__":
     scraper = TamilBlasters()
     threading = MultiThreading()
     print(
-        scraper.get_movie("Blast", "2026", threading)
+        scraper.get_movie("Bison: Kaalamaadan", "2026", threading)
     )
