ffe271a7633f93ad2ebbd53a370aa883938f243f
Jenish <jnz18.mail@gmail.com>
2026-06-21 15:21:55 +0530

    fixes

--- app/sources/anime/four_animo.py
+++ app/sources/anime/four_animo.py
@@ -35,7 +35,7 @@ class FourAnimoScraper(Scraper):
     
     def get_series(self, anilist_id: str, episode: str, stop_event: Optional[Event] = None) -> Optional[WebResponse]:
         url = f"{self.base_url}/api/embed/hd-1/ani/{anilist_id}/{episode}/sub?k=1&autoPlay=1"
-        result = self.get_stream(url, stop_event, title="Web | Anime")
+        result = self.get_stream(url, stop_event, title="Web | 4animo (Anime)")
         if result: 
             proxy_result = Proxy.get_proxy_url(result['url'], origin=self.base_url)
             if not proxy_result: return
