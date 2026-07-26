54a5a1c4896202af8d27a2e11e0dafd0e1c35a1b
Jenish <jnz18.mail@gmail.com>
2026-07-26 18:26:39 +0530

    logs

--- app/core/proxy.py
+++ app/core/proxy.py
@@ -346,7 +346,7 @@ class Proxy:
                     elapsed = time.monotonic() - start
 
                     speed = bytes_read / elapsed
-                    print(f"Speed: {speed / 1024} KB/s")
+                    print(f"Speed: {speed / 1024} KB/s".ljust(30), end="", flush=True)
 
                     if elapsed > 15 and speed < 100 * 1024:  # KB/s
                         if id: web_cache.switch_source(id)
