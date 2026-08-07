88b94bd0da9cebeda7b5b5d3b6201a7b0aaacba3
Jenish <jnz18.mail@gmail.com>
2026-08-07 19:00:38 +0530

    removed proxy

--- app/sources/regional/moviesda.py
+++ app/sources/regional/moviesda.py
@@ -9,7 +9,7 @@ from app.core.parsers import Parsers
 from app.models.metadata import Metadata
 import asyncio
 from app.core.scraper import Scraper
-from app.core.proxy import Proxy
+# from app.core.proxy import Proxy
 
 parsers = Parsers()
 
@@ -139,8 +139,8 @@ class Moviesda(Scraper):
         future = asyncio.run_coroutine_threadsafe(self.search_page(url), self._loop) # type: ignore
         responses = future.result(timeout=60)
 
-        for response in responses: 
-            response = Proxy.get_proxy_url(response)
+        # for response in responses: 
+        #     response = Proxy.get_proxy_url(response)
 
         return responses
         
