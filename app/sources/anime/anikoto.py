678c5f5c9ada2e9bb4c437312ea085271ab1d365
Jenish <jnz18.mail@gmail.com>
2026-09-10 00:38:34 +0530

    fixes

--- app/sources/anime/anikoto.py
+++ app/sources/anime/anikoto.py
@@ -11,14 +11,22 @@ class AnikotoScraper(Scraper):
     def __init__(self):
         super().__init__(base_url="https://megaplay.buzz", source="anikoto", stream_url_pattern=r'https?://\S*(?:master\.m3u8)\S*')
     
-    def get_series(self, mal_id: str, episode: str, stop_event: Optional[Event] = None) -> Optional[WebResponse]:
-        if not mal_id or not episode: return
-        url = f"{self.base_url}/stream/mal/{mal_id}/{episode}/sub"
-        result = self.get_stream(url, stop_event, title="Anikoto (Anime)")
-        return result
-    
+    def get_series(self, mal_id: str, episode: str, stop_event: Optional[Event] = None) -> Optional[list[WebResponse]]:
+        if not mal_id or not episode: return None
+        results: list[WebResponse] = []
+        url_sub = f"{self.base_url}/stream/mal/{mal_id}/{episode}/sub"
+        result_sub = self.get_stream(url_sub, stop_event, title="Anikoto (SUB)")
+        if result_sub:
+            results.append(result_sub)
+
+        url_dub = f"{self.base_url}/stream/mal/{mal_id}/{episode}/dub"
+        result_dub = self.get_stream(url_dub, stop_event, title="Anikoto (DUB)")
+        if result_dub:
+            results.append(result_dub)
+        return results if results else None
+
 
 if __name__ == "__main__":
     scraper = AnikotoScraper()
-    response = scraper.get_series("21", "1")
+    response = scraper.get_series("34034", "7")
     print(response)
