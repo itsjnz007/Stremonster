3bcaf25028df59811d3bb08037d856a002566153
Jenish <jnz18.mail@gmail.com>
2026-09-14 13:19:16 +0530

    fixes

--- app/core/extractors.py
+++ app/core/extractors.py
@@ -150,7 +150,7 @@ class StreamExtractor:
             (lambda tmdb, s, e: [result] if (result := viduki_scraper.get_series(tmdb, s, e)) else None, 'viduki'),
             (lambda tmdb, s, e: [result] if (result := videasy_scraper.get_series(tmdb, s, e)) else None, 'videasy'),
             (lambda tmdb, s, e: [result] if (result := vidnest_general_scraper.get_series(tmdb, s, e)) else None, 'vidlink'),
-            (lambda tmdb, s, e: [result] if (result := vidsrc_scraper.get_series(tmdb, s, e)) else None, 'vidsrc'),
+            # (lambda tmdb, s, e: [result] if (result := vidsrc_scraper.get_series(tmdb, s, e)) else None, 'vidsrc'),
             # (lambda tmdb, s, e: [result] if (result := fmovies_scraper.get_series(tmdb, s, e)) else None, 'fmovies'),
             # (lambda tmdb, s, e: [result] if (result := cineby_scraper.get_series(tmdb, s, e)) else None, 'cineby'),
             # (lambda tmdb, s, e: [result] if (result := flicky_scraper.get_series(tmdb, s, e)) else None, 'flicky'),
