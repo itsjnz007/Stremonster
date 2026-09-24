a0437ad1895d896da1072034aab88552d4d0094a
Jenish <jnz18.mail@gmail.com>
2026-09-24 22:27:13 +0530

    changes

--- app/core/proxy.py
+++ app/core/proxy.py
@@ -606,6 +606,6 @@ class Proxy:
             headers=sanitized_headers,
         )
 
-        logger.info(f"{upstream_response.status_code} | {round(time.time() - start_time, 2)} seconds | Streaming id {id} - origin {request.headers.get('Origin')} - range {request.headers.get('Range')} - url {media_url}")
+        logger.info(f"{upstream_response.status_code} | {round(time.time() - start_time, 2)} seconds | Streaming id {id} - origin {request.headers.get('Origin')} - range {request.headers.get('Range')}")
 
         return Proxy.apply_headers(resp)
