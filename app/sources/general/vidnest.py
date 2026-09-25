025bd1e70f9bf18d3b4c497f9e407682873fe9f6
Jenish <jnz18.mail@gmail.com>
2026-09-26 01:16:21 +0530

    addition

--- app/sources/general/vidnest.py
+++ app/sources/general/vidnest.py
@@ -10,7 +10,9 @@ from threading import Event
 class VidnestScraper(Scraper):
     def __init__(self):
         super().__init__(source="vidnest", base_url="https://vidnest.fun", 
-                         stream_url_pattern=r'https?://\S*(?:\.m3u8|\.mp4|/hls/|/stream/|/mp4|/videos/)\S*')
+                         stream_url_pattern=r'https?://\S*(?:\.m3u8|\.mp4|/hls/|/stream/|/mp4|/videos/)\S*',
+                        #  headless=False
+                         )
 
     def get_movie(self, tmdb_id: str, stop_event: Optional[Event] = None) -> Optional[WebResponse]:
         url = f"{self.base_url}/movie/{tmdb_id}"
